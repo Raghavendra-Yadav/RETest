e71893c32ec5a3c48a9d9cffe26e8f70026faa60
Raghavendra Yadav <graghavendra.yadav@gmail.com>
2024-02-21 13:00:05 -0500

    InsertOne

---
+++ InsertOne.py
@@ -0,0 +1,41 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from datetime import datetime, timezone
+import pprint 
+
+
+uri = "mongodb+srv://graghavendrayadav:Raghu@cluster0.gwjlwmk.mongodb.net/"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+
+    # Get reference to 'expenseTracking' database
+    db = client.expenseTracking
+
+    # Get reference to 'expenses' collection
+    collection = db.expenses
+
+    # inserting one expense
+    new_expense = {
+        "expense_logger": "Linus Torvalds",
+        "description": "Rent",
+        "amount": 1000,
+        "last_updated": datetime.now(timezone.utc),
+    }
+
+    # Expression that inserts the 'new_expense' document into the 'expenses' collection.
+    result = collection.insert_one(new_expense)
+
+    document_id = result.inserted_id
+    pprint.pprint(f"_id of inserted document: {document_id}")
+
+
+except Exception as e:
+    print(e)
+finally:
+    client.close()
+
